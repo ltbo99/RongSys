bdea54f117d513ed655ff8ec6feb2187a3bc4469
QwertyDF <381064474@qq.com>
2020-01-13 11:41:02 +0800

    修改上传文件

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/broad/PerController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/broad/PerController.java
@@ -111,10 +111,10 @@ public class PerController extends BaseController {
     public AjaxResult addSave(@RequestParam(value = "files") List<MultipartFile> files,@RequestParam(value = "uname", required = false) String uname) throws IOException {//大小
         String userid =  ShiroUtils.getSysUser().getUserId().toString();
         for(int i=0;i<files.size();i++){
-//            String duration = VideoUtil.ReadVideoTimeMs(files.get(i));
+            String duration = VideoUtil.ReadVideoTimeMs(files.get(i));
             String year = DateUtil.getYear();
             String maxfileid = iProgramService.getMaxFileidofYear(year);
-            Program g = bFileUtil.uplodeFile(maxfileid, files.get(i), files.get(i).getOriginalFilename(),"00:00:05", String.valueOf(files.get(i).getSize()), year, userid);
+            Program g = bFileUtil.uplodeFile(maxfileid, files.get(i), files.get(i).getOriginalFilename(),duration, String.valueOf(files.get(i).getSize()), year, userid);
             iProgramService.insertProgram(g);
         };
         return toAjax(1);
